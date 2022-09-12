a707c7b0b8496b14fa735428afadfa893f9392d5
casmagpily <90588086+casmagpily@users.noreply.github.com>
2022-09-12 08:58:33 +0800

    Update Q1_Exercise2.java

--- q1_exercise2/Q1_Exercise2.java
+++ q1_exercise2/Q1_Exercise2.java
@@ -23,22 +23,43 @@ public class Q1_Exercise2 {
             String initiate = "Start game";
             String config = "Change settings";
             String shutdown = "End application";
-            System.out.print("Welcome to Higher or Lower! What will you do?");
+            int guessNm = 3, lowerLimit = -4, upperLimit = 4, range;
+            range = Math.abs(lowerLimit) + Math.abs(upperLimit);
+            int ranNum = (int) Math.floor(Math.random()*range) + lowerLimit;
+            System.out.println("Welcome to Higher or Lower! What will you do?");
             System.out.println(initiate);
             System.out.println(config);
             System.out.println(shutdown);
             String response = sc.nextLine();
             switch(response){
                 case "Start game":
-                    for(int guessNm = 3;guessNm > 0;guessNm--){
-                        System.out.println("You have " + guessNm + "guess(es) left. What is your guess?");
+                    while(guessNm > 0){
+                        System.out.println("You have " + guessNm + " guess(es) left. What is your guess?");
+                        int guess = Integer.parseInt(sc.nextLine());
+                        guessNm--;
+                        if(guess < ranNum){
+                            System.out.println("Guess Higher! ");
+                        } else if(guess > ranNum){
+                            System.out.println("Guess Lower! ");
+                        } else if(guess == ranNum){
+                            System.out.println("Nice one! You got it!");
+                            break;
+                        }
+                        
+                    }
+                    if(guessNm == 0){
+                        System.out.println("You Lost... The answer was " + ranNum);
                     }
                     break;
                 case "Change settings":
-                    
+                    System.out.println("What is the lower limit of the random number?");
+                    System.out.println("What is the upper limit of the random number?");
+                    System.out.println("How many guesses are allowed?");
+                    System.out.println("Lower Limit:");
+                    lowerLimit = Integer.parseInt(sc.nextLine());
                     break;
-                case "End application":
-                    
+                case "End application":                   
+                    stop = true;
                     break;
                 
             }
