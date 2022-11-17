3829080a895eec4f30705416136e78bb0e1a1a47
casmagpily <90588086+casmagpily@users.noreply.github.com>
2022-11-17 13:39:05 +0800

    Update Q2_Exercise6.java

--- q2_exercise6/Q2_Exercise6.java
+++ q2_exercise6/Q2_Exercise6.java
@@ -15,7 +15,20 @@ public class Q2_Exercise6 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        SimpleMonster fire = new SimpleMonster("Brazodork", "Fire", "Grass", "Water", 20, 4);
+        FireType brazy = fire;
         
+        SimpleMonster water = new SimpleMonster("Drizzapear", "Water", "Fire", "Grass", 25, 4);
+        WaterType driz = water;
+        
+        SimpleMonster grass = new SimpleMonster("Lilacrie", "Grass", "Water", "Fire", 20, 5);
+        GrassType lilac = grass;
+        
+        lilac.fight(lilac, brazy);
+        
+        brazy.fight(brazy, driz);
+        
+        driz.battle(driz, lilac);
     }
     
 }
